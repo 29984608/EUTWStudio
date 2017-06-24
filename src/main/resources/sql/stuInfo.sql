431b23b19600dad4559ab4d04e9c29d5e404d6ab
Aceysx <aceysx@163.com>
2017-06-24 21:03:40 +0800

    更新个人工作记录

--- src/main/resources/sql/stuInfo.sql
+++ src/main/resources/sql/stuInfo.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 5.7.18, for Linux (x86_64)
 --
--- Host: 127.0.0.1    Database: stuInfo
+-- Host: 192.168.1.112    Database: stuinfo
 -- ------------------------------------------------------
--- Server version	5.7.18-0ubuntu0.16.10.1
+-- Server version	5.5.40
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -31,10 +31,10 @@ CREATE TABLE `t_permission` (
   `parentid` bigint(20) DEFAULT NULL COMMENT '父结点id',
   `parentids` varchar(128) DEFAULT NULL COMMENT '父结点id列表串',
   `sortstring` varchar(128) DEFAULT NULL COMMENT '排序号',
-  `available` char(1) DEFAULT NULL COMMENT '是否可用,1：可用，0不可用',
+  `available` varchar(10) DEFAULT NULL COMMENT '是否可用,1：可用，0不可用',
   `percode` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -43,7 +43,7 @@ CREATE TABLE `t_permission` (
 
 LOCK TABLES `t_permission` WRITE;
 /*!40000 ALTER TABLE `t_permission` DISABLE KEYS */;
-INSERT INTO `t_permission` VALUES (1,'权限',' ',NULL,NULL,0,'0/',NULL,'1',NULL),(2,'系统管理','menu','/permission','larry-xitongshezhi1',1,'0/1/',NULL,'1','permission:'),(3,'角色权限管理','menu','/rolePermission','',2,'0/1/2/',NULL,'1','rolePermission:manage'),(4,'角色权限删除','permission','/rolePermission/delete','',3,'0/1/2/3',NULL,'1','rolePermission:delete'),(5,'系统管理','permission','/rolePermission/','larry-xitongshezhi1',2,NULL,NULL,NULL,NULL);
+INSERT INTO `t_permission` VALUES (1,'权限',' ','',NULL,0,'0/',NULL,'1',NULL),(2,'系统管理','menu','/systemManage','larry-xitongshezhi1',1,'0/1/','1','1','permissionManage:'),(4,'添加','permission','','',6,'0/1/2/6/','3','1','userRole:add'),(5,'系统设置','menu','/systemSetting','larry-xitongshezhi2',2,'0/1/2/','2','1','systemSetting:'),(6,'用户角色分配','menu','/userRole','larry-yonghuliebiao',2,'0/1/2/','2','1','userRole:'),(11,'角色权限分配','menu','/rolePermission','larry-quanxianguanli',2,'0/1/2/','2','1','rolePermission:'),(12,'报表管理','menu','/portManage','larry-lanmuguanli1',1,'0/1/','1','1','portManage:'),(13,'学生报表','menu','/studentPort','larry-neirongguanli',12,'0/1/12/','2','1','studentPort:'),(14,'成绩报表','menu','/resultPort','larry-neirongguanli',12,'0/1/12/','2','1','resultPort:'),(15,'信息管理','menu','/infoManage','larry-lanmuguanli1',1,'0/1/','1','1','infoManage:'),(16,'学生信息','menu','/student','larry-gereninfo',15,'0/1/15/','2','1','student:'),(17,'老师信息','menu','/teacher','larry-gereninfo',15,'0/1/15/','2','1','teacher:'),(18,'沟通信息','menu','/communication','larry-neirong',15,'0/1/15/','2','1','communication:'),(19,'系信息','menu','/department','larry-jiankong1',15,'0/1/15/','2','1','department:'),(20,'专业信息','menu','/profession','larry-hrrq_xtrz',15,'0/1/15/','2','1','profession:'),(21,'课程信息','menu','/course','larry-hrrq_xtrz',15,'0/1/15/','2','1','course:'),(22,'修改','perimission','','',6,'0/1/2/6/','3','1','userRole:update');
 /*!40000 ALTER TABLE `t_permission` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -57,9 +57,9 @@ DROP TABLE IF EXISTS `t_role`;
 CREATE TABLE `t_role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(128) NOT NULL,
-  `available` char(1) DEFAULT NULL COMMENT '是否可用,1：可用，0不可用',
+  `available` varchar(10) DEFAULT NULL COMMENT '是否可用,1：可用，0不可用',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -68,7 +68,7 @@ CREATE TABLE `t_role` (
 
 LOCK TABLES `t_role` WRITE;
 /*!40000 ALTER TABLE `t_role` DISABLE KEYS */;
-INSERT INTO `t_role` VALUES (1,'管理员','1');
+INSERT INTO `t_role` VALUES (1,'管理员','true'),(2,'老师','false'),(3,'1233','true'),(4,'132','true'),(5,'1','2'),(6,'1231','true'),(7,'1231','true'),(8,'12312','true'),(9,'2342','true'),(10,'1231','true'),(11,'123213','true'),(12,'1231','true'),(13,'312','true'),(14,'1231','true'),(15,'2311','true'),(16,'1231','true'),(17,'12313','true'),(18,'1231','true'),(19,'1231','true'),(20,'1231','true'),(21,'33','true'),(22,'132','true'),(23,'23123','true'),(24,'1','2');
 /*!40000 ALTER TABLE `t_role` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -84,7 +84,7 @@ CREATE TABLE `t_role_permission` (
   `sys_role_id` varchar(36) NOT NULL COMMENT '角色id',
   `sys_permission_id` varchar(32) NOT NULL COMMENT '权限id',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -93,7 +93,7 @@ CREATE TABLE `t_role_permission` (
 
 LOCK TABLES `t_role_permission` WRITE;
 /*!40000 ALTER TABLE `t_role_permission` DISABLE KEYS */;
-INSERT INTO `t_role_permission` VALUES (1,'1','2'),(2,'1','3'),(3,'1','4');
+INSERT INTO `t_role_permission` VALUES (9,'1','2'),(10,'1','3'),(12,'1','5'),(13,'1','6'),(16,'1','11'),(17,'1','12'),(18,'1','13'),(19,'1','14'),(20,'1','15'),(21,'1','16'),(22,'1','17'),(23,'1','18'),(24,'1','19'),(25,'1','20'),(26,'1','21');
 /*!40000 ALTER TABLE `t_role_permission` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -117,7 +117,7 @@ CREATE TABLE `t_user` (
 
 LOCK TABLES `t_user` WRITE;
 /*!40000 ALTER TABLE `t_user` DISABLE KEYS */;
-INSERT INTO `t_user` VALUES ('111','111','1'),('admin','admin','1');
+INSERT INTO `t_user` VALUES ('111','111','2'),('admin','admin','1');
 /*!40000 ALTER TABLE `t_user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -130,4 +130,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-06-22 18:11:28
+-- Dump completed on 2017-06-24 20:40:41
