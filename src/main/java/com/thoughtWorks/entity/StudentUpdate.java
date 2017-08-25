e7792c269dc3d300f883f549ccf7d0b3c0069a2a
admin-m <977986310@qq.com>
2017-08-25 21:59:00 +0800

    添加显示学生状态

--- src/main/java/com/thoughtWorks/entity/StudentUpdate.java
+++ src/main/java/com/thoughtWorks/entity/StudentUpdate.java
@@ -68,6 +68,15 @@ public class StudentUpdate {
     private String arrears_second_year;
     private String teacher_id;
     private String community_teacher_id;
+    private String group;
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
 
     public String getNative_place() {
         return native_place;
